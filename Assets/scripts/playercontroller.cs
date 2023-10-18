4310aa36ebff10fac85e9481aa0ca8bc65517f51
7157laptops <99228263+7157laptops@users.noreply.github.com>
2023-10-17 17:17:03 -0700

    finished roll-a-ball tutorial and added speed limit and jumping (no infinite jumping)

--- Assets/scripts/playercontroller.cs
+++ Assets/scripts/playercontroller.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,12 +11,20 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     private float movementX;
     private float movementY;
-    public float speed = 0;
+    [SerializeField] float speed = 0;
+    bool onGround;
+    //[SerializeField] GameObject bullet;
+    int count;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
     // Start is called before the first frame update.
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
     }
 
     void OnMove(InputValue movementValue)
@@ -24,11 +33,70 @@ public class PlayerController : MonoBehaviour
 
         movementX = movementVector.x;
         movementY = movementVector.y;
+
+    }
+
+    void OnJump()
+    {
+        if (onGround)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, 5, rb.velocity.z);
+        }
+    }
+
+    /*void OnFire()
+    {
+        Instantiate(bullet, transform, true);
+    }*/
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        int collisionCount = collision.contactCount;
+        for (int i = 0; i < collisionCount; i++ )
+        {
+            Vector3 contactNormal = collision.contacts[i].normal;
+            bool isGround = Vector3.Dot(contactNormal, Vector3.up) > 0.9;
+            if (isGround)
+            {
+                onGround = true;
+                break;
+            }    
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        onGround = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
     }
 
     private void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);
+
+        //speed limit
+        if (rb.velocity.magnitude > 7)
+        {
+            rb.velocity = rb.velocity.normalized * 7;
+        }
     }
 }
